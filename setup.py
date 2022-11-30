5fe84667e1e195cc24d0ea0172912a1b7c4770c6
ogenio <91586850+ogenio@users.noreply.github.com>
2022-11-29 23:21:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ VERSION = __version__
 DESCRIPTION = open('README.md').read()
 AUTHOR = __author__
 AUTHOR_EMAIL = __email__
-URL = 'https://github.com/kauavpnlite/CheckUser'
+URL = 'https://github.com/ogenio/g'
 LICENSE = 'MIT'
 
 setup(name='CheckerUser',
